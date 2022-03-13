3752b7b08a97f124b133b061e1e921e540982638
EW-Shamp <ew_shamp@post.com>
2022-03-13 06:38:25 -0400

    Add Payment functions to JS

--- js/site.js
+++ js/site.js
@@ -5,16 +5,20 @@ function getValues() {
     let loanAmount = document.getElementById("loanAmount").value;
     let term = document.getElementById("term").value;
     let rate = document.getElementById("rate").value;
-    let valueObj = {}
+    let valueObj = {};
+    let paymentObj = {};
     //parse values
     loanAmount = parseInt(loanAmount);
     term = parseInt(term);
     rate = parseInt(rate);
     //verify the values are integers and pass them to 
-    //renderValues() function. If not alert the user
+    //renderValues() functions. If not alert the user
     //to enter a valid number
     if (Number.isInteger(loanAmount) && Number.isInteger(term) && Number.isInteger(rate)) {
-        valueObj = renderValues(loanAmount, term, rate);
+        valueObj.monthlyPayment = monthlyPayment(loanAmount, term, rate);
+        valueObj.totalCost = totalCost(valueObj.monthlyPayment, term);
+        valueObj.totalInterest = totalInterest(valueObj.totalCost, loanAmount);
+        paymentObj = payments(loanAmount, rate, term, valueObj.monthlyPayment);
     } else {
         alert("Please enter in only numbers");
     }
@@ -24,8 +28,49 @@ function getValues() {
 
 //render data from user
 //logic function(s)
-function renderValues(loanAmount, term, rate) {
-    
+
+//function to determine total monthly payment.
+function monthlyPayment(loanAmount, term, rate) {
+    let monthPayment = loanAmount * (rate / 1200) / (1 - (1 + rate / 1200) ** (-term));
+    monthPayment = monthPayment.toFixed(2);
+    return monthPayment;
+}
+
+//function to determine total loan cost
+function totalCost(payment, term) {
+    let cost = payment * term;
+    cost = cost.toFixed(2);
+    return cost;
+}
+
+//function to determine total interests
+function totalInterest(total, loanAmount) {
+    let interest = total - loanAmount;
+    interest = interest.toFixed(2);
+    return interest;
+}
+
+//function to determine interest payment, pricipal payment, and remaining balance
+function payments(amount, rate, term, monthlyPayment) {
+    let amt = amount;
+    let payments = {
+        interPayment: [],
+        princePayment: [],
+        balance: [],
+    };
+
+    for (i = term; i > 0; i--) {
+        let interPayment = amt * (rate / 1200);
+        let princePayment = monthlyPayment - interPayment;
+        interPayment = interPayment.toFixed(2);
+        princePayment = princePayment.toFixed(2);
+        amt = amt - princePayment;
+        amt = amt.toFixed(2)
+        payments.interPayment.push(interPayment);
+        payments.princePayment.push(princePayment);
+        payments.balance.push(amt)
+    }
+    return payments;
 }
 
 //display data to user
